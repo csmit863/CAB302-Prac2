9e26c8a0ec94f86d0c95d250ec8c5453f814d102
csmit863 <csmit863@outlook.com>
2024-07-31 13:51:43 +1000

    blah

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -2,10 +2,23 @@ public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
 
     // TODO Now: Add a constructor to initialize the users list with the default user
-
-    public static void main()
+    public static void main(String[] args){
+        ArrayList<User> users_list = new ArrayList<User>();
+    }
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
-
+    public User signUp(User newUser) {
+        if (users_list.contains(newUser)){
+            return null;
+        } else {
+            users_list.add(newUser);
+            return newUser;
+        }
+    }
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    public User logIn(User user_person){
+
+        if (user_list)
+        return username;
+    }
 }
